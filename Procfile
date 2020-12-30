69cd88ceb88cc010220e9c35c4dfd885ea1f721c
Vickie <Vickie.Blakelock@gmail.com>
2020-12-30 20:23:10 +0000

    installed dj_database_url, and psycopg2 to use postgress

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bouquetnblooms.wsgi:application
--- bouquetnblooms/settings.py
+++ bouquetnblooms/settings.py
@@ -16,6 +16,9 @@ from pathlib import Path
 if os.path.exists('env.py'):
     import env
 
+import dj_database_url
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -108,12 +111,17 @@ WSGI_APPLICATION = 'bouquetnblooms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
